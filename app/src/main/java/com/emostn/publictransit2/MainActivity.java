f55717a77683f8aab44bd5c61ab2098b6d890692
Emil Zdravkov <emostn@gmail.com>
2014-10-27 00:02:31 +0200

    Initial commit

---
+++ app/src/main/java/com/emostn/publictransit2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.emostn.publictransit2;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
